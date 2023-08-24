f0b787dab9af1b99faa6ca229af304e50d2618f3
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2023-08-24 11:43:32 +0000

    code

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfebc^1_2gbjc-@#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['adimuntean97-django-todo-app-329f05372d26.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
